e773b0644715328dcb7ec7d9aa15448aff56360b
Eduardo Luna <eolunas@gmail.com>
2024-01-29 23:04:06 -0500

    [ADD] Actions testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
+
 export default defineConfig({
   integrations: [tailwind(), preact()],
   site: "https://eolunas.github.io",
